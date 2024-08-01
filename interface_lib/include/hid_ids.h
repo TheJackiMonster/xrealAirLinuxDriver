67b215bf04bcac9da5853762425d6ce1f841452f
Jacki <jacki@thejackimonster.de>
2024-08-02 00:59:30 +0200

    Add license comments to all files
    Signed-off-by: Jacki <jacki@thejackimonster.de>

--- interface_lib/include/hid_ids.h
+++ interface_lib/include/hid_ids.h
@@ -1,3 +1,28 @@
+#pragma once
+//
+// Created by wheaney on 20.11.23.
+//
+// Copyright (c) 2023-2024 thejackimonster. All rights reserved.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+
 #ifndef __cplusplus
 #include <stdbool.h>
 #endif
@@ -10,7 +35,15 @@
 
 #define NUM_SUPPORTED_PRODUCTS 3
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 extern const uint16_t xreal_vendor_id;
 extern const uint16_t xreal_product_ids[NUM_SUPPORTED_PRODUCTS];
 
 bool is_xreal_product_id(uint16_t product_id);
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
